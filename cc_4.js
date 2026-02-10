2d6cbda12fea4e411d2185f0351441b71481b0c3
Briana Viscount <Brianaviscount@usf.edu>
2026-02-10 16:22:47 -0500

    first commit

---
+++ cc_4.js
@@ -0,0 +1,39 @@
+//step 2 
+let products =
+[
+    {
+    sku: "SKU-1234",
+    name: "laptop",
+    category: "electronics",
+    price: 349.99,
+    inventory: 40
+    },
+    {
+    sku: "SKU-2345",
+    name: "sweatshirt",
+    category: "apparel",
+    price: 29.99,
+    inventory: 55
+    },
+    {
+     sku: "SKU-3456",
+     name: "boots",
+     category: "apparel",
+     price: 39.99,
+     inventory: 20   
+    },
+    {
+    sku: "SKU-4567",
+    name: "keyboard",
+    category: "electronics",
+    pice: 25.99,
+    inventory: 35
+    },
+    {
+     sku: "SKU-5678",
+     name: "cake",
+     ctaegory: "groceries",
+     price: 49.99,
+     inventory: 15
+    }
+];
