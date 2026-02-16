b6e92e9118eccc9cd962f404e16720dea75da7e5
Briana Viscount <Brianaviscount@usf.edu>
2026-02-15 22:41:44 -0500

    Added Conditonals & Loops

--- cc_4.js
+++ cc_4.js
@@ -1,39 +1,111 @@
 //step 2 
-let products =
+const products =
 [
     {
-    sku: "SKU-1234",
     name: "laptop",
     category: "electronics",
     price: 349.99,
     inventory: 40
     },
     {
-    sku: "SKU-2345",
     name: "sweatshirt",
     category: "apparel",
     price: 29.99,
     inventory: 55
     },
     {
-     sku: "SKU-3456",
      name: "boots",
      category: "apparel",
      price: 39.99,
      inventory: 20   
     },
     {
-    sku: "SKU-4567",
     name: "keyboard",
     category: "electronics",
-    pice: 25.99,
+    price: 25.99,
     inventory: 35
     },
     {
-     sku: "SKU-5678",
+
      name: "cake",
-     ctaegory: "groceries",
+     category: "groceries",
      price: 49.99,
      inventory: 15
     }
 ];
+
+for (const product of products) {
+
+let discount= 0;
+
+switch (product.category) {
+    case "electronics":
+        discount = .2;
+        break;
+    case "apparel":
+        discount = .15;
+        break;
+    case "groceries":
+        discount = .1;
+        break;
+    case "household":
+        discount = .1;
+        break;
+        default:
+        break;
+        }
+product.promoprice = (product.price * (1 - discount)).toFixed(2);
+}
+//console.log(products);
+
+//step 4
+let customerType = "student";
+
+for (const product of products) {
+
+    let additionalDiscount = 0;
+if (customerType === "student"){
+   additionalDiscount = .05;
+}else if (customerType === "senior") {
+    additionalDiscount = .07;
+} else {
+    additionalDiscount = 0;
+}
+ product.finalprice = (product.promoprice * (1- additionalDiscount)).toFixed(2);
+
+}
+console.log(products);
+
+//step 5 
+//creating carts for 3 customers
+let carts = [
+{ 
+    customerType: "student", items: [products[0]]
+ }, 
+ {
+        customerType: "student", items: [products[1]]
+    },
+    {
+        customerType: "student", items: [products[2]]
+    }
+];
+
+
+
+for (let i=1; i <= 3; i++) {
+    let product = carts[i-1].items[0];
+    let total = product.finalprice;
+    product.inventory--;
+    console.log("customer "+ i + " total: " + total);
+} 
+//step 6 
+let product = products[0]
+for (const key in product) {
+    console.log(`${key}: ${product[key]}`);
+}
+//step 7 
+for (const product of products) {
+    for (const[key, value]of Object.entries(product)){
+        console.log(`${key}: ${value}`);
+    }
+}
